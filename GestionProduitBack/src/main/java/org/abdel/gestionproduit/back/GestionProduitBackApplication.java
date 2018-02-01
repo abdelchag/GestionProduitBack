594ec32a7c81431147ebf9717b2d4e282de6ae5a
ABDELCHAG <ABDELCHAG@DESKTOP-NFNHDVR>
2018-02-01 13:33:49 +0100

    Premier commit inititate projet

---
+++ GestionProduitBack/src/main/java/org/abdel/gestionproduit/back/GestionProduitBackApplication.java
@@ -0,0 +1,12 @@
+package org.abdel.gestionproduit.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitBackApplication.class, args);
+	}
+}
