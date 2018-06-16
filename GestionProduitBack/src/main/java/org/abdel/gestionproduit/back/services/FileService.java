80d0adf3ea9effbe318e2632b3bc6037f6f575f6
ABDELCHAG <abdellah.chagdani@gmail.com>
2018-06-16 12:44:45 +0200

    FIle  Download service

---
+++ GestionProduitBack/src/main/java/org/abdel/gestionproduit/back/services/FileService.java
@@ -0,0 +1,43 @@
+package org.abdel.gestionproduit.back.services;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/fileapi")
+@CrossOrigin(value="*")
+public class FileService {
+	
+	@RequestMapping(value = "/ping")
+	public String ping(){
+		return "wewe";
+	}
+	
+	@RequestMapping(value = "/download",method=RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<InputStreamResource> downloadFile() throws FileNotFoundException{
+		File file = new File("D:\\Documents\\test.txt");
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Disposition", "attachment; filename=test.txt");
+		headers.set("Content-type", MediaType.APPLICATION_OCTET_STREAM_VALUE);
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        InputStreamResource isr = new InputStreamResource(new FileInputStream(file));
+		ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(isr, headers, HttpStatus.OK);
+		return response;
+	}
+
+}
